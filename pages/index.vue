7a9d178c4fb25b38e72bb0e93e750b64bfda959f
dusanUTO <106300893+dusanUTO@users.noreply.github.com>
2022-05-30 14:46:13 +0200

    Add files via upload

---
+++ pages/index.vue
@@ -0,0 +1,156 @@
+<template>
+<div class="main bg-neutral-100">
+  <div class="w-full bg-cover" style="background-image:url('https://backpack.github.io/hero_155227bb4ce1b6bb859a04151cb1618c.png');"> 
+    <div class="container max-w-full xl:max-w-7xl mx-auto pt-20 pb-28 text-white px-10 2xl:px-0">
+        <div class="flex flex-wrap">
+          <img src="https://backpack.github.io/backpack-logo-sky-grey_353f3f6d9edb078851f2749527964197.svg" class="w-56 h-12 object-contain mr-10" alt="logo"/>
+          <p class="mt-5">Updated 27/05/2022</p>
+        </div>
+        <h4 class="font-medium leading-tight text-2xl mt-20 max-w-2xl">
+          <span aria-hidden="true">—</span>
+          <br>
+          Backpack is a collection of design resources, reusable components and guidelines for creating Skyscanner products.
+        </h4>
+    </div>
+  </div>  
+  <div class="container max-w-7xl mx-auto pb-28 px-10 2xl:px-0">
+        <div class="flex flex-wrap">
+          <div class="mb-3 w-full">
+            <input
+              type="search"
+              class="
+                form-control
+                block
+                w-full
+                h-14
+                px-3
+                py-1.5
+                text-base
+                font-normal
+                text-gray-700
+                bg-white bg-clip-padding
+                border border-solid border-gray-300
+                rounded
+                shadow-lg
+                transition
+                ease-in-out
+                -mt-7
+                focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
+              "
+              id="exampleSearch"
+              placeholder="Have a search first"
+            />
+          </div>
+       </div>
+    </div>  
+    <div class="container max-w-7xl mx-auto pb-16 px-10 2xl:px-0">
+        <div class="grid grid-cols-1 gap-x-0 gap-y-10 md:grid-cols-2 md:gap-10">
+            <div class="rounded-lg bg-white shadow-sm hover:shadow-xl transition-shadow">
+              <a href="#!">
+                <div class="relative">
+                  <img class="rounded-t-lg h-80 w-full object-cover" src="https://backpack.github.io/components_hero_0d1a0cbccdab05e116b6e5c1189487fe.jpg" alt=""/>
+                  <img src="~/assets/img/icon-1.svg" class="absolute top-1/2 -translate-y-1/2 left-16 w-16 h-16" />
+                </div>
+                <div class="p-6">
+                  <h5 class="text-gray-900 text-xl font-medium mb-2">Components</h5>
+                </div>
+              </a>
+            </div>            
+            <div class="rounded-lg bg-white shadow-sm hover:shadow-xl transition-shadow">
+              <a href="#!">
+                <img class="rounded-t-lg h-80 w-full object-cover" src="https://backpack.github.io/icons_4bd13c216d7babdb50cd0db025471f97.jpg" alt=""/>
+                <div class="p-6">
+                  <h5 class="text-gray-900 text-xl font-medium mb-2">Icons</h5>
+                </div>
+              </a>
+            </div>           
+            <div class="rounded-lg bg-white shadow-sm hover:shadow-xl transition-shadow md:col-span-2">
+              <a href="#!">
+                <div class="relative">
+                  <img class="rounded-t-lg h-80 w-full object-cover" src="https://backpack.github.io/design_tokens_1110bad6958da079c5442ac33c81ce81.jpg" alt=""/>
+                  <img src="~/assets/img/icon-2.svg" class="absolute top-1/2 -translate-y-1/2 left-32 w-16 h-16" />
+                </div>
+                <div class="p-6">
+                  <h5 class="text-gray-900 text-xl font-medium mb-2">Design tokens</h5>
+                </div>
+              </a>
+            </div>           
+            <div class="rounded-lg bg-white shadow-sm hover:shadow-xl transition-shadow">
+              <a href="#!">
+                <div class="relative">
+                  <img class="rounded-t-lg h-80 w-full object-cover" src="https://backpack.github.io/guidelines_455c44ff60e8ef46535693a902b147fe.png" alt=""/>
+                  <img src="~/assets/img/icon-3.svg" class="absolute top-1/2 -translate-y-1/2 left-20 w-16 h-16" />
+                </div>
+                <div class="p-6">
+                  <h5 class="text-gray-900 text-xl font-medium mb-2">Guidelines</h5>
+                </div>
+              </a>
+            </div>           
+            <div class="rounded-lg bg-white shadow-sm hover:shadow-xl transition-shadow">
+              <a href="#!">
+                <img class="rounded-t-lg h-80 w-full object-cover" src="https://backpack.github.io/using_bpk_0da184cf3b870356b58af85ff8399f08.jpg" alt=""/>
+                <div class="p-6">
+                  <h5 class="text-gray-900 text-xl font-medium mb-2">Using Backpack</h5>
+                </div>
+              </a>
+            </div>           
+            <div class="rounded-lg bg-white shadow-sm hover:shadow-xl transition-shadow">
+              <a href="#!">
+                <img class="rounded-t-lg h-80 w-full object-cover" src="https://backpack.github.io/sassdoc_d101b20f09bae0583176c50265f66b35.jpg" alt=""/>
+                <div class="p-6">
+                  <h5 class="text-gray-900 text-xl font-medium mb-2">SassDoc</h5>
+                </div>
+              </a>
+            </div>           
+            <div class="rounded-lg bg-white shadow-sm hover:shadow-xl transition-shadow">
+              <a href="#!">
+                <img class="rounded-t-lg h-80 w-full object-cover" src="https://backpack.github.io/request_27c737981bb9444aab19788448ba51b3.jpg" alt=""/>
+                <div class="p-6">
+                  <h5 class="text-gray-900 text-xl font-medium mb-2">Make a request</h5>
+                </div>
+              </a>
+            </div>
+        </div>
+    </div>
+  <footer class="bg-white">
+    <div class="max-w-7xl mx-auto pt-2 pb-2 flex flex-wrap md:justify-between px-10 2xl:px-0">
+      <div class="w-auto h-20 flex items-center flex-wrap">
+        Made with <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18" class="w-5 h-5 mr-2 ml-2 fill-blue-500" >
+                    <path d="M10.141 3.41a4.904 4.904 0 0 0-6.952.626 6.11 6.11 0 0 0-1.458 5.859c.898 4.03 5.543 8.553 9.051 11.415a1.905 1.905 0 0 0 2.436 0c3.508-2.862 8.153-7.384 9.05-11.415a6.106 6.106 0 0 0-1.457-5.86 4.904 4.904 0 0 0-6.952-.624l-1.18 1.03a1.025 1.025 0 0 1-1.357 0l-1.18-1.03z" />
+                  </svg> by <a href="#!" class="mr-1 ml-1 text-blue-600 hover:underline">Skyscanner</a> © 2022
+      </div>
+      <div class="w-auto h-20 flex flex-wrap items-center">
+        <a href="#!" class="mr-1 ml-1 text-blue-600 hover:underline">Baseline grid on</a> | 
+        <a href="#!" class="mr-1 ml-1 text-blue-600 hover:underline">RTL on</a>
+        <select class="form-select appearance-none
+              block
+              w-auto
+              px-3
+              py-1.5
+              text-base
+              font-normal
+              text-gray-700
+              bg-white bg-clip-padding bg-no-repeat
+              border border-solid border-gray-300
+              rounded
+              transition
+              ease-in-out
+              m-0
+              focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" aria-label="Default select example"
+              style="background: #fff url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNCAyNCIgd2lkdGg9IjE4IiBoZWlnaHQ9IjE4Ij48c3R5bGU+PC9zdHlsZT48cGF0aCBkPSJNMTYuNTM3IDguMjVINy40NjNhMS4zNTggMS4zNTggMCAwMC0xLjExIDIuMjUxbDQuMzU0IDQuNzdhMS41MyAxLjUzIDAgMDAyLjE4NC4wNGw0LjcxOC00Ljc3YTEuMzU3IDEuMzU3IDAgMDAtMS4wNzItMi4yOTF6IiBmaWxsPSIjNDQ0NTYwIi8+PC9zdmc+) no-repeat right 0.5rem center;">
+                <option selected>None</option>
+                <option value="London">London</option>
+                <option value="Hong Kong">Hong Kong</option>
+                <option value="Doha">Doha</option>
+            </select>
+      </div>
+    </div>
+  </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
